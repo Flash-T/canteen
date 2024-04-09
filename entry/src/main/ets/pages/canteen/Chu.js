cfec92622e311f4c68b62de95492e78d5ec6091e
traplash <1938282676@qq.com>
2024-04-09 12:50:15 +0800

    init

---
+++ entry/src/main/ets/pages/canteen/Chu.js
@@ -0,0 +1,8 @@
+"use strict";
+struct Chu extends   {
+    constructor() { }
+    build() {
+            .height('100%');
+    }
+}
+//# sourceMappingURL=Chu.js.map
---
+++ entry/src/main/ets/pages/canteen/Han.js
@@ -0,0 +1,8 @@
+"use strict";
+struct Han extends   {
+    constructor() { }
+    build() {
+            .height('100%');
+    }
+}
+//# sourceMappingURL=Han.js.map
